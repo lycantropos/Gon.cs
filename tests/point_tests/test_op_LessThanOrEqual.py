3c1fcce9d20b29a0f8fa52313f5d252042afa280
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-09 04:53:59 +0200

    Add test for 'Point.op_LessThanOrEqual' method

---
+++ tests/point_tests/test_op_LessThanOrEqual.py
@@ -0,0 +1,32 @@
+from hypothesis import given
+
+from tests.binding import Point
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.points)
+def test_reflexivity(point: Point) -> None:
+    assert point <= point
+
+
+@given(strategies.points, strategies.points)
+def test_antisymmetry(first: Point, second: Point) -> None:
+    assert equivalence(first <= second <= first, first == second)
+
+
+@given(strategies.points, strategies.points, strategies.points)
+def test_transitivity(first: Point, second: Point, third: Point) -> None:
+    assert implication(first <= second <= third, first <= third)
+
+
+@given(strategies.points, strategies.points)
+def test_alternatives(first: Point, second: Point) -> None:
+    assert equivalence(first <= second, first < second or first == second)
+    assert equivalence(first <= second, second > first or first == second)
+    assert equivalence(first <= second, first < second or not first != second)
+    assert equivalence(first <= second, second > first or not first != second)
+    assert equivalence(first <= second, second >= first)
+    assert equivalence(first <= second, not second < first)
+    assert equivalence(first <= second, not first > second)
