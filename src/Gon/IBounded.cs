520aeab7978adc0eda60a79d43ca3e48a20e56ad
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-16 00:35:15 +0200

    Fix 'IDE0055: Fix formatting'

--- src/Gon/IBounded.cs
+++ src/Gon/IBounded.cs
@@ -5,9 +5,6 @@ namespace Gon
     internal interface IBounded<Scalar>
         where Scalar : IEquatable<Scalar>
     {
-#if NETCOREAPP3_0_OR_GREATER
-        public
-#endif
         Box<Scalar> BoundingBox { get; }
     }
 }
