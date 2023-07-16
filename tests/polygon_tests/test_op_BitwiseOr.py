4e8a64c67d1108aa88591f44352e8820a2beb68c
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-17 01:40:08 +0200

    Complete test of 'Polygon.op_BitwiseOr' method

--- tests/polygon_tests/test_op_BitwiseOr.py
+++ tests/polygon_tests/test_op_BitwiseOr.py
@@ -1,14 +1,16 @@
 from hypothesis import given
 
 from tests.binding import Polygon
+from tests.hints import Shaped
 from tests.utils import (are_polygons_sequences_equivalent,
-                         reverse_polygon_coordinates)
+                         reverse_polygon_coordinates,
+                         reverse_shaped_coordinates)
 from . import strategies
 
 
-@given(strategies.polygons, strategies.polygons)
-def test_basic(first: Polygon, second: Polygon) -> None:
-    result = first | second
+@given(strategies.polygons, strategies.shaped_geometries)
+def test_basic(polygon: Polygon, shaped: Shaped) -> None:
+    result = polygon | shaped
 
     assert isinstance(result, list)
     assert all(isinstance(element, Polygon) for element in result)
@@ -24,12 +26,12 @@ def test_commutativity(first: Polygon, second: Polygon) -> None:
     assert are_polygons_sequences_equivalent(first | second, second | first)
 
 
-@given(strategies.polygons, strategies.polygons)
-def test_reversals(first: Polygon, second: Polygon) -> None:
-    result = first | second
+@given(strategies.polygons, strategies.shaped_geometries)
+def test_reversals(polygon: Polygon, shaped: Polygon) -> None:
+    result = polygon | shaped
 
     assert are_polygons_sequences_equivalent(
             result, [reverse_polygon_coordinates(polygon)
-                     for polygon in (reverse_polygon_coordinates(first)
-                                     | reverse_polygon_coordinates(second))]
+                     for polygon in (reverse_polygon_coordinates(polygon)
+                                     | reverse_shaped_coordinates(shaped))]
     )
