ddb1987eea379e2f67146a5130006fc97c74ef27
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-17 03:24:50 +0200

    Add test for 'Polygon.Locate' method

---
+++ tests/polygon_tests/test_Locate.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+from tests.binding import Location, Polygon
+from . import strategies
+
+
+@given(strategies.polygons)
+def test_vertices(polygon: Polygon) -> None:
+    assert all(polygon.locate(vertex) is Location.BOUNDARY
+               for vertex in polygon.border.vertices)
+    assert all(polygon.locate(vertex) is Location.BOUNDARY
+               for hole in polygon.holes
+               for vertex in hole.vertices)
