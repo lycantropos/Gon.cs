2c6aeabe0511cf3fcaa943a32dedad92af355348
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-17 04:08:59 +0200

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from datetime import timedelta
 
 import pytest
@@ -7,10 +6,7 @@ from hypothesis import (HealthCheck,
                         settings)
 
 on_ci = bool(os.getenv('CI', False))
-is_pypy = sys.implementation.name == 'pypy'
-max_examples = ((1 if is_pypy else 10)
-                if on_ci
-                else settings.default.max_examples)
+max_examples = 10 if on_ci else settings.default.max_examples
 settings.register_profile('default',
                           max_examples=max_examples,
                           deadline=(timedelta(hours=1) / max_examples
